0bdda9bba30fec2128f1293a02279f011e710a25
unknown <adj.hamzaoui@gmail.com>
2024-06-07 00:42:55 +0200

    create router-recipe

---
+++ server/app/routers/api/recipeRouter.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const router = express.Router();
+
+
+const { read, readOneById} = require("../../controllers/recipeActions");
+
+router.get("/",read);
+
+router.get("/:id", readOneById);
+
+module.exports = router
