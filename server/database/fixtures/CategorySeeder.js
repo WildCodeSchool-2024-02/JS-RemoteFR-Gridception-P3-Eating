3d5eab6db554362b02a9cf058f8b760247376769
unknown <adj.hamzaoui@gmail.com>
2024-06-11 16:21:56 +0200

    create recipeseeder and set faker

---
+++ server/database/fixtures/CategorySeeder.js
@@ -0,0 +1,22 @@
+const AbstractSeeder = require("./AbstractSeeder");
+
+class CategorySeeder extends AbstractSeeder {
+    constructor() {
+        super({ table: "category", truncate: true });
+    }
+
+    run() {
+        for (let i = 0; i < 10; i += 1) {
+
+            const fakeCategory = {
+                name: this.faker.commerce.product(),
+                refName: `category_${i}`
+            }
+
+            this.insert(fakeCategory)
+        }
+    }
+}
+
+
+module.exports = CategorySeeder;
