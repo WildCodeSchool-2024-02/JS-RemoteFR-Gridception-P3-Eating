04f4a961315d93d78cd8ba2aef5792163b7a7405
Benoît Vandanjon <benoit@vandanjon.fr>
2024-06-12 10:02:44 +0200

    prettier fixes

--- server/database/fixtures/CategorySeeder.js
+++ server/database/fixtures/CategorySeeder.js
@@ -1,32 +1,31 @@
 const AbstractSeeder = require("./AbstractSeeder");
 
 class CategorySeeder extends AbstractSeeder {
-    constructor() {
-        super({ table: "category", truncate: true });
-        this.categories = [
-            "Plats rapides",
-            "Plats végétariens",
-            "Plats végétaliens",
-            "Salades",
-            "Soupes",
-            "Smoothies et boissons",
-            "Desserts",
-            "Plats sans gluten",
-            "Petit-déjeuners sains",
-            "Recettes riches en protéines"
-        ]
-    }
+  constructor() {
+    super({ table: "category", truncate: true });
+    this.categories = [
+      "Plats rapides",
+      "Plats végétariens",
+      "Plats végétaliens",
+      "Salades",
+      "Soupes",
+      "Smoothies et boissons",
+      "Desserts",
+      "Plats sans gluten",
+      "Petit-déjeuners sains",
+      "Recettes riches en protéines",
+    ];
+  }
 
-    run() {
-        for (let i = 0; i < this.categories.length; i += 1) {
-            const listOfCategory = {
-                name: this.categories[i],
-            }
+  run() {
+    for (let i = 0; i < this.categories.length; i += 1) {
+      const listOfCategory = {
+        name: this.categories[i],
+      };
 
-            this.insert(listOfCategory)
-        }
+      this.insert(listOfCategory);
     }
+  }
 }
-
 
 module.exports = CategorySeeder;
--- server/database/tables.js
+++ server/database/tables.js
@@ -1,17 +1,18 @@
-
 const CategoryRepository = require("./models/CategoryRepository");
-const IngredientRepository = require("./models/IngredientRepository")
+const IngredientRepository = require("./models/IngredientRepository");
+const UserRepository = require("./models/UserRepository");
+const RoleRepository = require("./models/RoleRepository");
 
 const tables = {};
 
 tables.category = new CategoryRepository();
 tables.ingredient = new IngredientRepository();
+tables.user = new UserRepository();
+tables.role = new RoleRepository();
 
 module.exports = new Proxy(tables, {
   get(obj, prop) {
-
     if (prop in obj) return obj[prop];
-
 
     throw new ReferenceError(
       `tables.${prop} is not defined. Did you register it in ${__filename}?`
