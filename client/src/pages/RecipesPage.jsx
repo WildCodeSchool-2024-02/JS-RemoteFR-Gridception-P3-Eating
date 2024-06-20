f3aa0e0bd268a79faec5ac4725e15ce612f6a9c4
Lauryn MARTIN <lauryn.martin333@gmail.com>
2024-06-20 16:23:06 +0200

    Supp du slider dans RecipesPage et ajout du lien dans la navBar

--- client/src/pages/RecipesPage.jsx
+++ client/src/pages/RecipesPage.jsx
@@ -1,32 +1,10 @@
-// import { useState } from "react";
-//  import { useEffect } from "react";
-
 import { useLoaderData, Link } from "react-router-dom";
 
 import "../style/RecipesPage.css";
 
-// import required modules
-
 function RecipesPage() {
   console.info(useLoaderData());
   const dataRecipes = useLoaderData();
-  // const [slides, setSlides] = useState(0);
-  //   const [recipes, setRecipes] = useState([]);
-
-  // const setSlidesPerview = () => {
-  //   // eslint-disable-next-line no-nested-ternary
-  //   setSlides(window.innerWidth < 640 ? 1 : window.innerWidth >= 640 ? 3 : 0);
-  // };
-
-  // useEffect(() => {
-  //   setSlidesPerview();
-  //   setRecipes(dataRecipes);
-  //   window.addEventListener("resize", setSlidesPerview);
-
-  //   return () => {
-  //     window.removeEventListener("resize", setSlidesPerview);
-  //   };
-  // }, []);
 
   return (
     <section className="recipes-page">
@@ -38,24 +16,20 @@ function RecipesPage() {
         />
       </div>
 
-      <swiper-container
-        pagination="true"
-        pagination-clickable="true"
-        grid-rows="3"
-        space-between="30"
-        centered-slides="true"
-        // slides-per-view={slides}
-        loop="true"
-      >
+      <div className="recipes-container">
         {dataRecipes.map((recipe) => (
-          <swiper-slide key={recipe.id}>
+          <div className="card-recipes" key={recipe.id}>
             <Link key={recipe.id} to={`/RecipePage/${recipe.id}`}>
-              <img src={recipe.image} alt={recipe.title} />
+              <img
+                className="image-slider-recipies"
+                src={recipe.image}
+                alt={recipe.title}
+              />
             </Link>
             <h2>{recipe.title}</h2>
-          </swiper-slide>
+          </div>
         ))}
-      </swiper-container>
+      </div>
     </section>
   );
 }
