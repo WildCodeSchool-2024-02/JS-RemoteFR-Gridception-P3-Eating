9c23ddc6bc87f45f76eb1f5e5f8b406a133500aa
unknown <adj.hamzaoui@gmail.com>
2024-06-21 14:43:02 +0200

    fix classNames modifications

--- client/src/pages/CommentCaMarche.jsx
+++ client/src/pages/CommentCaMarche.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import "../styles/ccm.css";
+import "../styles/comment_ca_marche.css";
 import deco1 from "../assets/images/deco1.png";
 import deco2 from "../assets/images/deco2.png";
 import deco3 from "../assets/images/deco3.png";
@@ -7,56 +7,60 @@ import deco4 from "../assets/images/deco4.png";
 import flecheGauche from "../assets/images/flecheGauche.png";
 import flecheDroite from "../assets/images/flecheDroite.png";
 import flecheMilieu from "../assets/images/flecheMilieu.png";
-import blé from "../assets/images/blé.png";
+import ble from "../assets/images/ble.png";
 
 function CommentCaMarche() {
   return (
     <>
-      <h1 className="pageTitleCcm">Comment ça marche</h1>
-      <img className="flecheGaucheCcm" src={flecheGauche} alt="" />
-      <div className="ContentCcm">
-        <section className="column1Ccm">
-          <div className="oneCcm">
+      <h1 className="page-title-ccm">Comment ça marche</h1>
+      <img
+        className="fleche-gauche-ccm"
+        src={flecheGauche}
+        alt="flecheGauche"
+      />
+      <div className="content-ccm">
+        <section className="column1-ccm">
+          <div className="one-ccm">
             <h2>UNE RECETTE</h2>
             <p>Je trouve la recette de mes rêves</p>
-            <img className="img1Ccm" src={deco1} alt="deco1" />
+            <img className="img1-ccm" src={deco1} alt="deco1" />
           </div>
-          <div className="twoCcm">
+          <div className="two-ccm">
             <h2>UN CLIC</h2>
             <p>Je me connecte avec mon compte</p>
-            <img className="img2Ccm" src={deco2} alt="deco2" />
+            <img className="img2-ccm" src={deco2} alt="deco2" />
           </div>
           <img
-            className="flecheMilieuCcm"
+            className="fleche-milieu-ccm"
             src={flecheMilieu}
-            alt="flècheMilieu"
+            alt="fleche-milieu"
           />
         </section>
 
-        <section className="column2Ccm">
-          <div className="threeCcm">
+        <section className="column2-ccm">
+          <div className="three-ccm">
             <h2>SELECTIONNE</h2>
             <p>Je sélectionne la recette de mon choix</p>
-            <img className="img3Ccm" src={deco3} alt="deco3" />
+            <img className="img3-ccm" src={deco3} alt="deco3" />
           </div>
-          <div className="fourCcm">
+          <div className="four-ccm">
             <h2>CUISINE</h2>
             <p>Je cuisine ce que j'ai choisi</p>
-            <img className="img4Ccm" src={deco4} alt="deco4" />
+            <img className="img4-ccm" src={deco4} alt="deco4" />
           </div>
           <img
-            className="flecheDroiteCcm"
+            className="fleche-droite-ccm"
             src={flecheDroite}
             alt="flecheDroite"
           />
         </section>
       </div>
-      <div className="containerButtonCcm">
-        <Link className="buttonCcm" to="/RecipePage">
+      <div className="container-button-ccm">
+        <Link className="button-ccm" to="/RecipePage">
           Recettes
         </Link>
       </div>
-      <img className="BleCcm" src={blé} alt="ble" />
+      <img className="ble-ccm" src={ble} alt="ble" />
     </>
   );
 }
