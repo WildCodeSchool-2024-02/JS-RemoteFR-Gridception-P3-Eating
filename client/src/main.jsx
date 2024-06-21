10ff0bd2090195e831e9b387c7d8d52b4420bdc6
BigLZN <lznpro51@gmail.com>
2024-06-21 11:34:17 +0200

    Front Login fini

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Login from "./pages/login";
 
 import App from "./App";
 
@@ -10,6 +10,7 @@ const router = createBrowserRouter([
     path: "/",
     element: <App />,
   },
+  { path: "/login", element: <Login /> },
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
---
+++ client/src/pages/login.jsx
@@ -0,0 +1,89 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+import "./styles/login.css";
+
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [showSuccessModal, setShowSuccessModal] = useState(false);
+  const [showErrorModal, setShowErrorModal] = useState(false);
+
+  const closeSuccessModal = () => setShowSuccessModal(false);
+  const closeErrorModal = () => setShowErrorModal(false);
+
+  return (
+    <>
+      <div>
+        <div className="background">
+          <form method="post">
+            <section>
+              <div className="cardcenter">
+                <img
+                  src="../../../src/assets/images/graille.png"
+                  alt="welcomeimg"
+                  className="imgwelcome"
+                />
+                <div className="cardlogin">
+                  <p className="bienvenuetexte">de retour ?</p>
+                  <hr />
+                  <p className="logintext">
+                    Entrez vos informations de{" "}
+                    <span className="connexiontext">connexion</span>
+                  </p>
+                  <div data-mdb-input-init className="form-group">
+                    <input
+                      type="email"
+                      id="typeEmailX"
+                      name="email"
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                      placeholder="Email"
+                      required
+                    />
+                  </div>
+                  <div data-mdb-input-init className="form-group">
+                    <input
+                      type="password"
+                      id="typePasswordX"
+                      name="password"
+                      placeholder="Mot de passe"
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                      required
+                    />
+                  </div>
+                  <button
+                    data-mdb-button-init
+                    data-mdb-ripple-init
+                    type="submit"
+                    className="connexionbutton"
+                  >
+                    Connexion
+                  </button>
+                  <p className="logintext">
+                    Vous n'avez pas encore de compte ?
+                    <Link to="/register">
+                      <span className="button">S'inscrire</span>
+                    </Link>
+                  </p>
+                </div>
+              </div>
+            </section>
+          </form>
+        </div>
+      </div>
+
+      {showErrorModal && (
+        <div className="modal">
+          <div className="modal-content">
+            <button type="button" className="close" onClick={closeErrorModal}>
+              &times;
+            </button>
+            <p>Échec de connexion ! Essayez à nouveau.</p>
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
