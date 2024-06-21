79dacd7319e5b6056ce4d29a33a7b17efa6e1874
BonnevieAmel <amel.bonnevie@gmail.com>
2024-06-21 14:21:28 +0200

    loader recipe in main ok

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -18,12 +18,18 @@ const router = createBrowserRouter([
       {
         element: <RecipePage />,
         path: "/RecipePage/:id",
-        loader: async () => {
+        loader: async ({ params }) => {
+          const { id } = params;
+
+          if (!id) {
+            throw new Error("ID de recette non défini");
+          }
+
           const recipesResponse = await fetch(
             "http://localhost:3310/api/recipes"
           );
           const quantityResponse = await fetch(
-            "http://localhost:3310/api/quantities/recipe/1"
+            `http://localhost:3310/api/quantities/recipe/${id}`
           );
           const recipesData = await recipesResponse.json();
           const quantityData = await quantityResponse.json();
