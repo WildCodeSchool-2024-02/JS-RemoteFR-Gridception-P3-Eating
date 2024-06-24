da98a0b2c238d2c6966f2a1afd52d7864dff9b07
unknown <adj.hamzaoui@gmail.com>
2024-06-25 00:47:20 +0200

    add a bread but it don't work

--- server/app/controllers/categoryActions.js
+++ server/app/controllers/categoryActions.js
@@ -1,24 +1,75 @@
-const tables = require("../../database/tables");
+const CategoryRepository = require('../../database/models/CategoryRepository');
 
-const read = async (req, res, next) => {
+const categoryRepo = new CategoryRepository();
+
+const browse = async (req, res, next ) => {
   try {
-    const categories = await tables.category.read();
+    const categories = await categoryRepo.browse();
     res.json(categories);
   } catch (error) {
     next(error);
   }
 };
 
-const readOneById = async (req, res, next) => {
+const read = async (req, res, next) => {
   try {
-    const category = await tables.category.readOneById(req.params.id);
-    res.json(category);
-  } catch (error) {
-    next(error);
-  }
-};
+    const category = await categoryRepo.read(req.params.id);
+    if (category) {
+      res.json(category);
+      } else {
+        res.status(404).json({ message: 'Category not found'});
+      }
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  const add = async (req, res, next) => {
+    const newCategoryData = req.body;
+    try {
+      const insertedId = await categoryRepo.add(newCategoryData);
+      const insertedCategory = { id: insertedId, ...newCategoryData };
+      res.status(201).json(insertedCategory);
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  const edit = async (req, res, next) => {
+    const { id } = req.params;
+    const updatedCategoryData = req.body;
+    try {
+      const success = await categoryRepo.edit(req.params.id, updatedCategoryData);
+      if (success) {
+        const updatedCategory = { id, ...updatedCategoryData };
+        res.json(updatedCategory);
+      } else {
+        res.status(404).json({ message: 'Category not found' });
+      }
+    } catch(error) {
+      next(error);
+    }
+  };
+
+  const deleteCategory = async (req, res, next) => {
+    const { id } = req.params;
+    try {
+      const success = await categoryRepo.delete(id);
+      if (success) {
+        res.json({ message: 'Category deleted successfully' });
+      } else {
+        res.status(404).json({ message: 'Category not found' });
+      }
+    } catch (error) {
+      next(error);
+    }
+  };
+
 
 module.exports = {
+  browse,
   read,
-  readOneById,
+  add,
+  edit,
+  deleteCategory
 };
