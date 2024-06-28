69c452cefdbd7d108e3507cd39f278770febb87f
Lauryn MARTIN <lauryn.martin333@gmail.com>
2024-06-28 11:45:05 +0200

    Search Bar dans RecipesPage ok

--- client/src/pages/RecipesPage.jsx
+++ client/src/pages/RecipesPage.jsx
@@ -1,9 +1,15 @@
 import { useLoaderData, Link } from "react-router-dom";
+import { useState } from "react";
 
 import "../styles/RecipesPage.css";
 
 function RecipesPage() {
   const dataRecipes = useLoaderData();
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleSearchTerm = (e) => {
+    setSearchTerm(e.target.value);
+  };
 
   return (
     <section className="recipes-page">
@@ -12,22 +18,27 @@ function RecipesPage() {
           className="search-input"
           type="text"
           placeholder="Rechercher une recette"
+          onChange={handleSearchTerm}
         />
       </div>
 
       <div className="recipes-container">
-        {dataRecipes.map((recipe) => (
-          <div className="card-recipes" key={recipe.id}>
-            <Link key={recipe.id} to={`/RecipePage/${recipe.id}`}>
-              <img
-                className="image-slider-recipies"
-                src={recipe.image}
-                alt={recipe.title}
-              />
-            </Link>
-            <h2>{recipe.title}</h2>
-          </div>
-        ))}
+        {dataRecipes
+          .filter((recipe) =>
+            recipe.title.toLowerCase().includes(searchTerm.toLowerCase())
+          )
+          .map((recipe) => (
+            <div className="card-recipes" key={recipe.id}>
+              <Link key={recipe.id} to={`/RecipePage/${recipe.id}`}>
+                <img
+                  className="image-slider-recipies"
+                  src={recipe.image}
+                  alt={recipe.title}
+                />
+              </Link>
+              <h2>{recipe.title}</h2>
+            </div>
+          ))}
       </div>
     </section>
   );
