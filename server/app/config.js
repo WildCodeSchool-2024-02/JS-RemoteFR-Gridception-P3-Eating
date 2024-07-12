c5fc95155ab0c3db5d55e63161f58467f883799a
Benoît V <82836739+Vandanjon@users.noreply.github.com>
2024-07-12 12:39:01 +0200

    Update config.js

--- server/app/config.js
+++ server/app/config.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
 
@@ -13,5 +14,23 @@ app.use(express.json());
 const apiRouter = require("./routers/api/router");
 
 app.use("/api", apiRouter);
+
+
+const reactBuildPath = path.join(__dirname, "/../../client/dist");
+const publicFolderPath = path.join(__dirname, "/../public");
+
+// Serve react resources
+
+app.use(express.static(reactBuildPath));
+
+// Serve server resources
+
+app.get("*.*", express.static(publicFolderPath, { maxAge: "1y" }));
+
+// Redirect unhandled requests to the react index file
+
+app.get("*", (_, res) => {
+  res.sendFile(path.join(reactBuildPath, "/index.html"));
+});
 
 module.exports = app;
